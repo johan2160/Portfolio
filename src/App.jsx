87e331d9cb3edfce60d93094dc78262b9d4eda82
johandev21 <johan.p.2160@gmail.com>
2024-07-25 20:45:16 -0400

    fix: change prop name text to lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ const App = () => {
 
   return (
     <div className='font-poppins bg-background selection:bg-selectionbg selection:text-selectiontxt'>
-      <Navbar name="JOHANDEV21" link1="Info" link2="Proyectos" link3="Contacto"></Navbar>
+      <Navbar name="johandev21" link1="Info" link2="Proyectos" link3="Contacto"></Navbar>
       <Info name="Johan Carrasco" profession="Ingeniero en Informática" btnText="Descargar CV"></Info>
       <Skills/>
       <Portfolio></Portfolio>
       <Contactme></Contactme>
-      <Footer name="JOHANDEV21" link1="Info" link2="Proyectos" link3="Contacto"></Footer>
+      <Footer name="johandev21" link1="Info" link2="Proyectos" link3="Contacto"></Footer>
     </div>
   )
 }
