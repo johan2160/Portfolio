2509edf786a71e3dc29043739d7c243a1f9d57d3
johandev21 <johan.p.2160@gmail.com>
2024-07-25 20:48:45 -0400

    fix(button): remove outline-none from Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({text}) => {
   return (
     <>
-      <button type='submit' className='bg-primary outline-none text-background w-full max-w-max px-[25px] py-3 rounded-md text-xl font-medium hover:bg-background hover:text-primary border-2 border-transparent hover:border-primary transition-colors active:bg-[#26221ff0]'>{text}</button>
+      <button type='submit' className='bg-primary  text-background w-full max-w-max px-[25px] py-3 rounded-md text-xl font-medium hover:bg-background hover:text-primary border-2 border-transparent hover:border-primary transition-colors active:bg-[#26221ff0]'>{text}</button>
     </>
   )
 }
