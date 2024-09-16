ef295b4e0766670daf69fca708185d6cfe915c98
johandev21 <johan.p.2160@gmail.com>
2024-09-16 12:39:14 -0300

    feat: update portfolio message to Spanish
    Updated the portfolio thank you message from English to Spanish.
    Changed "Thanks for taking the time of navigating my Portfolio 🎊" to "Gracias por tomarte el tiempo de navegar por mi portafolio 🎊".

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({name}) => {
     <footer className='flex flex-col items-center gap-4 px-4 py-5 mt-64 text-center text-content2 md:mt-80 xl:mt-96 xl:py-10 bg-primary'>
       <div className='w-full flex flex-col gap-6 md:gap-8 items-center xl:flex-row xl:justify-between max-w-[1200px]'>
         <h3 className='text-2xl'>{name}</h3>
-        <p>Thanks for taking the time of navigating my Portfolio 🎊</p>
+        <p>Gracias por tomarte el tiempo de navegar por mi portafolio 🎊</p>
         <ul className='flex gap-6'>
             <a href='https://github.com/johan2160' target='_blank'><RiGithubFill className='transition-colors text-content2 hover:text-selectionbg' /></a>
             <a href='https://www.linkedin.com/in/johan-carrasco-493a85319' target='_blank'><RiLinkedinBoxFill className='transition-colors text-content2 hover:text-selectionbg' /></a>
