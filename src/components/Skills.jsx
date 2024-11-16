b451c23e73726f2917018478e7f42589d4084740
johandev21 <johan.p.2160@gmail.com>
2024-11-16 20:05:39 -0300

    refactor(skills): update skills component and optimize icons
    - Removed unused imports (`RiBootstrapFill`, `RiGithubFill`, `MongoDB`).
    - Replaced MongoDB icon with Django to better reflect the technology stack.
    - Adjusted SVG import paths for compatibility.
    - Simplified and streamlined the JSX structure for improved readability.
    - Ensured consistency in hover effects and class usage across all icons.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Marquee from "react-fast-marquee";
-import { RiBootstrapFill, RiCss3Fill, RiGithubFill, RiHtml5Fill, RiJavascriptFill, RiReactjsFill, RiTailwindCssFill } from '@remixicon/react';
-import Python from '../../public/assets/icons/python.svg';
-import MongoDB from '../../public/assets/icons/mongodb.svg';
-import MySQL from '../../public/assets/icons/mysql.svg';
+import { RiCss3Fill, RiHtml5Fill, RiJavascriptFill, RiReactjsFill, RiTailwindCssFill } from '@remixicon/react';
+import Python from '/assets/icons/python.svg';
+import Django from '/assets/icons/django.svg';
+import MySQL from '/assets/icons/mysql.svg';
 
 const Skills = () => {
   const [iconSize, setIconSize] = useState(64);
@@ -33,15 +33,13 @@ const Skills = () => {
       <Marquee pauseOnHover={true} gradient={true} gradientColor='text-black' gradientWidth={150} speed={90} className='rounded-md bg-background max-w-[1176px] md:w-11/12'>
         <div className='flex py-4'>
           <img width={iconSize} src={Python} alt=""  className='mx-6 transition-all md:mx-6 xl:mx-8 hover:scale-110'/>
+          <img width={iconSize} src={Django} alt=""  className='mx-6 transition-all md:mx-6 xl:mx-8 hover:scale-110'/>
           <RiHtml5Fill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-htmlbg hover:scale-110'/>
           <RiCss3Fill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-cssbg hover:scale-110'/>
           <RiJavascriptFill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-jsbg hover:scale-110'/>
-          <RiGithubFill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-githubcolor hover:scale-110'/>
-          <img width={iconSize} src={MongoDB} alt=""  className='mx-6 transition-all md:mx-6 xl:mx-8 hover:scale-110'/>
           <RiReactjsFill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-reactcolor hover:scale-110'/>
           <img width={iconSize} src={MySQL} alt=""  className='mx-6 transition-all md:mx-6 xl:mx-8 hover:scale-110'/>
           <RiTailwindCssFill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-tailwindcolor hover:scale-110'/>
-          <RiBootstrapFill size={iconSize} className='mx-6 transition-all md:mx-6 xl:mx-8 text-bootstrapbg hover:scale-110'/>
         </div>
       </Marquee>
     </div>
