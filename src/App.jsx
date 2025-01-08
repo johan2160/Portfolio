0b425e431bec3e9af13fb8d2300659422e89f019
johandev21 <johan.p.2160@gmail.com>
2025-01-08 20:01:26 -0300

    feat(app): wrap App component with ThemeProvider for improved theme management

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import InfoSection from './components/Info'
-import ProjectsSection from './components/Projects'
-import ContactMeSection from './components/ContactMe'
-import SkillsSection from './components/Skills'
-import Footer from './components/Footer'
+import React from "react";
+import Navbar from "./components/Navbar";
+import InfoSection from "./components/Info";
+import ProjectsSection from "./components/Projects";
+import ContactMeSection from "./components/ContactMe";
+import SkillsSection from "./components/Skills";
+import Footer from "./components/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const App = () => {
-
   return (
-    <div className='font-poppins bg-background selection:bg-selectionbg selection:text-selectiontxt'>
-      <Navbar name="johandev21" link1="Info" link2="Proyectos" link3="Contacto"></Navbar>
-      <InfoSection name="Johan Carrasco" profession="Desarrollador Web" btnText="Descargar CV"></InfoSection>
-      <SkillsSection/>
-      <ProjectsSection></ProjectsSection>
-      <ContactMeSection></ContactMeSection>
-      <Footer name="johandev21" link1="Info" link2="Proyectos" link3="Contacto"></Footer>
-    </div>
-  )
-}
+    <ThemeProvider>
+      <div className="font-poppins bg-background selection:bg-selectionbg selection:text-selectiontxt">
+        <Navbar
+          name="johandev21"
+          link1="Info"
+          link2="Proyectos"
+          link3="Contacto"
+        ></Navbar>
+        <InfoSection
+          name="Johan Carrasco"
+          profession="Desarrollador Web"
+          btnText="Descargar CV"
+        ></InfoSection>
+        <SkillsSection />
+        <ProjectsSection></ProjectsSection>
+        <ContactMeSection></ContactMeSection>
+        <Footer
+          name="johandev21"
+          link1="Info"
+          link2="Proyectos"
+          link3="Contacto"
+        ></Footer>
+      </div>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
